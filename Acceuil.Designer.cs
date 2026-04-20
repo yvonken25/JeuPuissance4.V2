26733f2684b691a9d6098d47fdbc8c159c27fa36
yvonken25 <129590870+yvonken25@users.noreply.github.com>
2026-04-20 23:33:52 +0200

    Ajout Acceuil

---
+++ Acceuil.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace JeuPuissance4
+{
+    partial class Acceuil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bJouer = new Button();
+            SuspendLayout();
+            // 
+            // bJouer
+            // 
+            bJouer.BackColor = SystemColors.ButtonShadow;
+            bJouer.Cursor = Cursors.Hand;
+            bJouer.Location = new Point(770, 315);
+            bJouer.Name = "bJouer";
+            bJouer.Size = new Size(199, 68);
+            bJouer.TabIndex = 0;
+            bJouer.Text = "Nouvelle Partie";
+            bJouer.UseVisualStyleBackColor = false;
+            bJouer.Click += bJouer_Click;
+            // 
+            // Acceuil
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.p4;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1012, 580);
+            Controls.Add(bJouer);
+            Name = "Acceuil";
+            Text = "Acceuil";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button bJouer;
+    }
+}
---
+++ Acceuil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeuPuissance4
+{
+    public partial class Acceuil : Form
+    {
+        public Acceuil()
+        {
+            InitializeComponent();
+        }
+
+        private void bJouer_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            f.Show();
+        }
+    }
+}
